484af45f72cf5e90b9ecdc28f180a6d42fd86573
Anup Ghatage <aghatage@salesforce.com>
2024-01-16 15:06:12 -0800

    Set ChunkedEncoding to false.

--- src/main/java/org/example/DependencyFactory.java
+++ src/main/java/org/example/DependencyFactory.java
@@ -3,6 +3,7 @@ package org.example;
 
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Configuration;
 
 import java.net.URI;
 
@@ -17,8 +18,13 @@ public class DependencyFactory {
      * @return an instance of S3Client
      */
     public static S3AsyncClient s3Client() {
+
+        S3Configuration configuration = S3Configuration.builder()
+                .chunkedEncodingEnabled(false)
+                .build();
         return S3AsyncClient.builder()
                 .endpointOverride(URI.create("http://127.0.0.1:9000"))
+                .serviceConfiguration(configuration)
                 .credentialsProvider(ProfileCredentialsProvider.create())
                 .build();
     }
